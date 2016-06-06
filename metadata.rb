83c6a9a2baf92782dbca090f54251c4a55381036
Ryan Moon <ryanmoon@users.noreply.github.com>
2016-06-06 11:27:55 -0700

    ryanmoon-chef-cookbook-template MASTER
      - inital-commit

---
+++ metadata.rb
@@ -0,0 +1,5 @@
+name             'ryanmoon-template'
+description      'Custom code generator cookbook for use with ChefDK'
+long_description 'Custom code generator cookbook for use with ChefDK'
+version          '0.1.1'
+
